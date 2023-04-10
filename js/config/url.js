fd982b7f439f7746f1bc9dd1620df958f259bdbc
daniferdinandall <daniferdinandall@gmail.com>
2023-04-11 06:16:35 +0700

    update

---
+++ js/config/url.js
@@ -0,0 +1,5 @@
+export let urlAPI = "https://ws-dani.herokuapp.com/dhs-all";
+export let urlAPIdhs = "https://ws-dani.herokuapp.com/dhs-all";
+export let urlAPImatkul = "https://ws-dani.herokuapp.com/matkul-all";
+export let urlAPIdosen = "https://ws-dani.herokuapp.com/dosen-all";
+export let urlAPImahasiswa = "https://ws-dani.herokuapp.com/mahasiswa-all";
---
+++ js/temp/table.js
@@ -0,0 +1,41 @@
+export let isiTabel =
+    `
+<tr class="border-b dark:border-neutral-500">
+    <td class="whitespace-nowrap px-6 py-4 font-medium">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#NAMA#</p>
+            <p class="text-xs font-medium text-coolGray-500">#NPM#</p>
+        </div>
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#PROGRAM_STUDI#</p>
+            <p class="text-xs font-medium text-coolGray-500">#FAKULTAS#</p>
+        </div>
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#MATKUL#</p>
+            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI#</p>
+            </div>
+        </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#MATKUL1#</p>
+            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI1#</p>
+        </div>
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#MATKUL2#</p>
+            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI2#</p>
+        </div>
+    </td>
+    <td class="whitespace-nowrap px-6 py-4">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#MATKUL3#</p>
+            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI3#</p>
+        </div>
+    </td>
+</tr>
+`
