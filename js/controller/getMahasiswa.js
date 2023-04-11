d2d851f8c1e2c5b8ea82be73c49a258600e93127
daniferdinandall <daniferdinandall@gmail.com>
2023-04-11 07:17:52 +0700

    update

--- js/controller/getMahasiswa.js
+++ js/controller/getMahasiswa.js
@@ -1,6 +1,6 @@
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 // import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
-import { isiTabel } from "../temp/table.js";
+import { isiTabelMahasiswa } from "../temp/table.js";
 export function isiTablePresensi(results) {
     results.forEach(isiRow);
 }
@@ -8,7 +8,7 @@ function isiRow(value) {
     console.log(value)
     console.log(value.mata_kuliah?value.mata_kuliah[1].nama:"1")
     let content =
-        isiTabel.replace("#NAMA#", value.nama)
+        isiTabelMahasiswa.replace("#NAMA#", value.nama)
             .replace("#NPM#", value.npm)
             .replace("#PROGRAM_STUDI#", value.program_studi?value.program_studi.nama:"#PROGRAM_STUDI#")
             .replace("#FAKULTAS#", value.fakultas?value.fakultas.nama:"#FAKULTAS#")
